66b22107dd49b0b6f81bcb8e97328775a130c79c
Kaan <kkureli@gmail.com>
2020-05-11 16:46:03 +0300

    todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
+// interface ITodo2 extends ITodo {
+//   tags: string[] //aray of strings
+// }
+
+const App = () => {
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]); //ITodo iceren bir array olcak, array of objects(ITodo) olacak, objects will be Itododa belirtilen string ve boolena olacak.
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ToDo List</h1>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            type="text"
+            required
+          />
+          <button type="submit">Add Todo</button>
+        </form>
+        <section>
+          {todos.map((todo: ITodo, index: number) => (
+            <div key={index}>
+              <span
+                style={{
+                  textDecoration: todo.complete ? "line-through" : "",
+                }}
+              >
+                {todo.text}
+              </span>
+              <button onClick={() => completeTodo(index)}>
+                {todo.complete ? "Done" : "Not Done"}
+              </button>
+              <button onClick={() => removeTodo(index)}>Remove</button>
+            </div>
+          ))}
+        </section>
       </header>
     </div>
   );
-}
+};
 
 export default App;
